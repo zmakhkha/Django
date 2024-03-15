d40ef4fd36ebfbe4a4497d7ff5644d3c77a1b34e
Zakaria Makhkhas <zmakhkha@e2r6p6.1337.ma>
2024-03-15 13:49:43 +0000

    using sql in django

--- python/playground/urls.py
+++ python/playground/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('hello/', views.hello)
+	path('hello/', views.hello),
+	path('tfoo/', views.hello),
+	path('lala/', views.hello),
+	path('no/', views.hello),
 ]
--- python/storefront/settings.py
+++ python/storefront/settings.py
@@ -78,21 +78,21 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'storefront',
-#         'HOST': 'localhost',
-#         'USER': 'root',
-#         'PASSWORD': 'root_password',
-    # }
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'db',
+        'USER': 'root',
+        'PASSWORD': 'root_password',
+    }
+}
 
 
 # Password validation
